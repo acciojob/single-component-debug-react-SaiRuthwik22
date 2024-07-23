748c73242065367936f057f984389df2f0479d15
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-07-23 08:16:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 function App()
 {
-  return(
-    <div id="main">
-      <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-    </div>
-  )
+    return (
+        <div id="main">
+            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        </div>
+    );
 }
 export default App
